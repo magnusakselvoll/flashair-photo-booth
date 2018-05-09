38ccd24af76dd38e12e352748962853b629503d1
Magnus Akselvoll <magnus@akselvoll.net>
2018-05-09 20:57:40 +0200

    Added release

---
+++ releases/Create-Release.ps1
@@ -0,0 +1,15 @@
+param(
+    [Parameter(Mandatory=$false)]
+    [String]$version = 'current'
+)
+
+if (Test-Path $version)
+{
+    Remove-Item -Path $version -Recurse
+}
+
+New-Item -Path "$version" -ItemType Directory
+New-Item -Path "$version\flashair-downloader" -ItemType Directory
+New-Item -Path "$version\flashair-slideshow" -ItemType Directory
+Copy-Item -Path "..\flashair-downloader\Start-FlashAirDownloader.ps1" -Destination "$version\flashair-downloader"
+Copy-Item -Path "..\flashair-slideshow\bin\Release\*" -Destination "$version\flashair-slideshow"

e07c4b027f4859f62c6a16ef276eb41b0d2ddbe5
Magnus Akselvoll <magnus@akselvoll.net>
2018-05-10 01:07:35 +0200

    Added utility for auto-connecting to wifi

--- releases/Create-Release.ps1
+++ releases/Create-Release.ps1
@@ -11,5 +11,7 @@ if (Test-Path $version)
 New-Item -Path "$version" -ItemType Directory
 New-Item -Path "$version\flashair-downloader" -ItemType Directory
 New-Item -Path "$version\flashair-slideshow" -ItemType Directory
+New-Item -Path "$version\wifi-utilities" -ItemType Directory
 Copy-Item -Path "..\flashair-downloader\Start-FlashAirDownloader.ps1" -Destination "$version\flashair-downloader"
 Copy-Item -Path "..\flashair-slideshow\bin\Release\*" -Destination "$version\flashair-slideshow"
+Copy-Item -Path "..\wifi-utilities\Connect-WifiAuto.ps1" -Destination "$version\wifi-utilities"
---
+++ wifi-utilities/Connect-WifiAuto.ps1
@@ -0,0 +1,80 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [String]$InterfaceName,
+    [Parameter(Mandatory=$true)]
+    [String]$WifiProfile,
+    [Parameter(Mandatory=$false)]
+    [timespan]$RefreshInterval = (New-TimeSpan -Seconds 30),
+    [Parameter(Mandatory=$false)]
+    [timespan]$MaximumExecutionTime = (New-TimeSpan -Hours 24)
+)
+
+function Invoke-NetShellWlan
+{
+    param([string[]] $Params)
+
+    $result = &{netsh.exe wlan $Params}
+
+    return $result
+}
+
+Write-Host "Attempting to maintain connection to profile $WifiProfile on interface $InterfaceName every $RefreshInterval";
+
+$stopwatch =  [system.diagnostics.stopwatch]::StartNew()
+$secondsToSleep = $([int]($RefreshInterval.TotalSeconds))
+$iteration = 0
+$reconnects = 0
+
+while ($stopwatch.Elapsed -lt $MaximumExecutionTime)
+{
+    $iteration++
+    Write-Host "Starting iteration $iteration" -ForegroundColor Green
+
+    $consoleLines = Invoke-NetShellWlan "show", "interfaces"
+
+    $consoleLines | Write-Verbose
+    $interfaceDetected = $false
+    $connected = $false
+
+    foreach ($line in $consoleLines)
+    {
+        $parts = $line.Split(':')
+        if ($parts.Length -ne 2)
+        {
+            continue
+        }
+
+        $key = $parts[0].Trim()
+        $value = $parts[1].Trim()
+
+        if ($key -eq 'Name')
+        {
+            $interfaceDetected = ($value -eq $InterfaceName)
+            continue
+        }
+
+        if (-not $interfaceDetected)
+        {
+            continue
+        }
+
+        if ($key -eq 'State' -and $value -eq 'connected')
+        {
+            $connected = $true
+            break;
+        }
+    }
+
+    Write-Host "Interface $InterfaceName connected: $connected"
+
+    if (-not $connected)
+    {
+        Invoke-NetShellWlan "connect", "name=$WifiProfile", "interface=$InterfaceName" | Write-Verbose
+        Write-Host 'Attempted reconnection'
+        $reconnects++
+    }
+
+    Write-Host "Finished iteration $iteration. Reconnects: $reconnects. Sleeping for $secondsToSleep s." -ForegroundColor Green
+    Write-Host ''
+    Start-Sleep -Seconds $secondsToSleep
+}
